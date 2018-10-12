e219f61813ea6ce2a773bcd1b742c941903cf590
wenbo <2851820735@qq.com>
2018-10-12 16:34:05 +0800

    公共资源编写

--- src/main/java/com/springboot/mybatis/SpringbootWithMybatisApplication.java
+++ src/main/java/com/springboot/mybatis/SpringbootWithMybatisApplication.java
@@ -1,9 +1,11 @@
 package com.springboot.mybatis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.springboot.mybatis.dao.mapper")
 public class SpringbootWithMybatisApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/springboot/mybatis/dao/Student.java
@@ -0,0 +1,12 @@
+package com.springboot.mybatis.dao;
+
+public class Student{
+    private int id;
+    private int age;
+    private int course_id;
+    private String hometown;
+    private String name;
+    private int number;
+    private String telephone;
+
+}
