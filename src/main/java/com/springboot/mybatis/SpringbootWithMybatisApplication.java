c0280fadd2cde92e2d244f5154fd133084d6e31e
wenbo <2851820735@qq.com>
2018-10-12 16:13:16 +0800

    Initial commit with mybatis configure

---
+++ src/main/java/com/springboot/mybatis/SpringbootWithMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWithMybatisApplication.class, args);
+    }
+}
